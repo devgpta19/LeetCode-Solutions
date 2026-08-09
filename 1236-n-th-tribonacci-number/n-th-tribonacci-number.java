7e8909bfc24d21362b4ed80365b8bbd12f9d8ad1
Devansh Gupta <80698697+devgpta19@users.noreply.github.com>
2026-08-09 17:48:46 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (72.19%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int tribonacci(int n) {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int sum = 0;
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        if(n == 2) return 1;
+        if(n == 3) return 2;
+        while(n-2!=0){
+            sum = a + b + c;
+            a = b;
+            b = c;
+            c = sum;
+            n--;
+        }
+        return sum;
+    }
+}
